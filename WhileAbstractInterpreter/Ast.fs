30b3b4850a2d690088c55db8a73dbd529a1e8138
Davide Albiero <davide2910@live.it>
2023-04-01 18:34:51 +0200

    First commit

---
+++ WhileAbstractInterpreter/Ast.fs
@@ -0,0 +1,17 @@
+﻿module Ast
+
+type Expr =
+    | Num of int
+    | Variable of string
+    | Boolean of bool
+    | UnOp of string * Expr
+    | BinOp of Expr * string * Expr
+
+type Stm =
+    | VarDec of string * Expr
+    | Skip
+    | IfThenElse of Expr * Stm * Stm
+    | While of Expr * Stm
+//    | Read of string //Capire come implementare la lettura e se serve
+//    | Print of Expr
+    | Seq of Stm * Stm
---
+++ WhileAbstractInterpreter/Program.fs
@@ -0,0 +1,24 @@
+﻿module Program
+
+open System
+open FSharp.Text.Lexing
+
+let evaluate input =
+    let lexbuf = LexBuffer<char>.FromString input
+    Parser.prog Lexer.tokenize lexbuf
+
+
+[<EntryPoint>]
+let main args =
+    let input = """
+    // Example from page 109 of the slides.
+    x := -1;
+    while x != 0 do {
+      x := x + 1
+    }
+    """
+
+    Console.WriteLine(input)
+    let result = evaluate input
+    Console.WriteLine(result)
+    0
