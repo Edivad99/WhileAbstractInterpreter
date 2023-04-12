ca669b1932c0fcc3c62f9cd2270294ebe8c01762
Davide Albiero <davide2910@live.it>
2023-04-12 13:10:32 +0200

    Sistemata la lettura da file

--- WhileAbstractInterpreter/Examples/test.wl
+++
@@ -1,5 +0,0 @@
-﻿// Example from page 109 of the slides.
-x := -1;
-while x != 0 do {
-  x := x + 1
-}
--- WhileAbstractInterpreter/Program.fs
+++ WhileAbstractInterpreter/Program.fs
@@ -5,6 +5,7 @@ open System.IO
 open System.Reflection
 open FSharp.Text.Lexing
 open IntervalDomain
+open SignDomain
 open Domain
 open AbstractState
 
@@ -12,27 +13,31 @@ let evaluate input =
     let lexbuf = LexBuffer<char>.FromString input
     Parser.prog Lexer.tokenize lexbuf
 
-let evaluate_file filename =
-    let executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
-    let fileLocation = Path.Combine(executableLocation, "Examples", filename)
-
-    use rd = new StreamReader(fileLocation)
-    let lexbuf = LexBuffer<char>.FromTextReader rd
-    Parser.prog Lexer.tokenize lexbuf
+let read_file filename =
+    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+    |> fun x -> Path.Combine (x, "Examples", filename)
+    |> File.ReadAllText
 
 
 [<EntryPoint>]
 let main args =
     let input = """
-        x := 110;
-        y := 0;
+        x := -1;
+        if 0 > x then {
+            x := 10
+        } else {
+            x := -10
+        }
     """
+    //let input = read_file "if.wl"
+
 
     let program = evaluate input 
-    //let program = evaluate_file "test.wl"
     Console.WriteLine(program)
 
     let interval_domain = IntervalDomain()
+    let sign_domain = SignDomain()
+
     let abstract_state = AbstractState<_>(interval_domain)
     let init_state = interval_domain.get_init_state program
     let (result, program_points) = abstract_state.eval (program, init_state, [init_state])
